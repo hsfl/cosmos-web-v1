2d12acd6dbb47c5648f5e9852939d50bc8f663a5
Willy Chang <willychang@rocketmail.com>
2020-07-17 10:08:07 -1000

    chore: add documentation

--- src/components/Dashboard/Activity.jsx
+++ src/components/Dashboard/Activity.jsx
@@ -7,7 +7,8 @@ import dayjs from 'dayjs';
 import BaseComponent from '../BaseComponent';
 
 /**
- * Retrieves data from a web socket. Displays an event along with the timestamp in a table.
+ * Shows the incoming activity from the web socket and displays time elapsed
+ * from the last data retrieval.
  */
 function Activity({
   height,
@@ -15,18 +16,29 @@ function Activity({
   /** Get agent list state from the Context */
   const activities = useSelector((s) => s.activity);
 
+  /** Color of the indicator, initial state is red */
   const [color, setColor] = useState('red');
+
+  /** Keeps track of whether to show all agents (active & inactive) or just active ones */
   const [toggle, setToggle] = useState(true);
-  const [elapsedTime, setElapsedTime] = useState('Over a day ago');
+
+  /** The modified information coming in from the state activities, includes time elapsed */
   const [data, setData] = useState([]);
 
+  /** Reference to the timer that changes the indicator to yellow */
   const timerYellow = useRef(null);
+
+  /** Reference to the timer that changes the indicator to red */
   const timerRed = useRef(null);
+
+  /** Reference to the timer that increments all elapsed times after 1 second */
   const timer = useRef(null);
 
-  // Get lastDate (last retrieved date) and compare with current time)
-  // Compare. If <2 min, green, <5min, yellow, < 10min, red
-
+  /**
+   * Calculate the difference between the current time and the date provided, returning
+   * the difference as a dayjs object. If the difference is over a day, then a string is returned.
+   * @param {dayjs object} date
+   */
   const getDiff = (date) => {
     if (typeof date !== 'string' && dayjs().diff(date, 'day') < 1) {
       const hour = dayjs().diff(date, 'hour');
@@ -37,8 +49,13 @@ function Activity({
     return 'Over a day ago';
   };
 
+  /**
+   * Store the incoming activity in data with the elapsed field and calculate the difference.
+   * Start the timers for the indicator color change.
+   */
   useEffect(() => {
     if (activities && activities.length !== 0) {
+      /** Store incoming activity in a new array with the elapsed field */
       setData((d) => [
         {
           status: activities[0].status,
@@ -50,19 +67,23 @@ function Activity({
         ...d,
       ]);
 
+      /** Calculate difference in times */
       const minuteDifference = activities[0].time.diff(dayjs(), 'minute');
       if (-minuteDifference <= 2) {
+        /** Reset 1 second timer */
         if (timer.current != null) {
           clearTimeout(timer.current);
         }
 
         setColor('green');
 
+        /** Reset timers */
         if (timerYellow.current != null && timerRed.current != null) {
           clearTimeout(timerYellow.current);
           clearTimeout(timerRed.current);
         }
 
+        /** Start timers */
         timerYellow.current = setTimeout(() => {
           setColor('orange');
         }, 300000);
@@ -73,8 +94,10 @@ function Activity({
     }
   }, [activities]);
 
+  /** Increments the timers for all data points */
   useEffect(() => {
     if (data != null) {
+      /** Set the 1 second timer */
       timer.current = setTimeout(() => {
         setData(data.map((point) => ({
           status: point.status,
@@ -86,6 +109,7 @@ function Activity({
       }, 1000);
     }
 
+    /** Clear timer on unmount */
     return () => clearTimeout(timer.current);
   }, [data]);
 
@@ -138,11 +162,9 @@ function Activity({
             <table>
               <tbody>
                 {
-                // eslint-disable-next-line camelcase
                 data ? data.map(({
                   status, summary, scope, time, elapsed,
                 }) => (
-                  // eslint-disable-next-line camelcase
                   <tr className="truncate ..." key={summary + time + scope}>
                     <td>
                       <Badge status={status} />
