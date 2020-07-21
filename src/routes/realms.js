d543bd58c9ec05c58f834bd652a1032781cdda09
Willy Chang <willychang@rocketmail.com>
2020-07-20 18:25:43 -1000

    style: separated date and time in navbar

--- src/routes/realms.js
+++ src/routes/realms.js
@@ -11,7 +11,7 @@ export default {
   props: {
     defaultLayout,
     realms: {
-      neutron1: ['neutron1', 'beagle1'],
+      neutron1: ['neutron1', 'beagle1', 'virtualhost.hsfl.hawaii.edu'],
       hiapo: [],
       hyti: [],
     },
