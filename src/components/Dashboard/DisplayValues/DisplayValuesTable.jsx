488821c1f413909a8b093a8ffad540a5343520fe
spjy <spjy@hawaii.edu>
2020-08-12 13:30:31 -1000

    feat: add namespace help info

--- src/components/Dashboard/DisplayValues/DisplayValuesTable.jsx
+++ src/components/Dashboard/DisplayValues/DisplayValuesTable.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Popover } from 'antd';
+
 function DisplayValuesTable({
   displayValues,
 }) {
@@ -15,23 +17,58 @@ function DisplayValuesTable({
             displayValues.map((
               {
                 name,
+                nodeProcess,
+                dataKey,
+                processDataKey,
                 dataKeyLowerThreshold,
                 dataKeyUpperThreshold,
+                timeDataKey,
                 value,
                 unit,
                 time,
               },
-              i,
             ) => (
-              <tr key={JSON.stringify(displayValues[i])}>
-                <td className="pr-2 text-gray-500 text-right">
-                  { name }
-                </td>
+              <tr key={`${name}${nodeProcess}${dataKey}${timeDataKey}${processDataKey.toString()}${unit}`}>
+                <Popover
+                  className="cursor-pointer"
+                  title={name}
+                  trigger="click"
+                  placement="left"
+                  content={(
+                    <>
+                      <div className="text-xs text-gray-500">
+                        Node
+                      </div>
+                      { nodeProcess }
+                      <div className="text-xs text-gray-500 pt-2">
+                        Namespace Key
+                      </div>
+                      { dataKey }
+                      <div className="text-xs text-gray-500 pt-2">
+                        Namespace Time Key
+                      </div>
+                      { timeDataKey }
+                      <div className="text-xs text-gray-500 pt-2">
+                        Limits
+                      </div>
+                      { dataKeyLowerThreshold || '-∞' }
+                      &nbsp;
+                      &le;&nbsp;
+                      x&nbsp;
+                      &le;&nbsp;
+                      { dataKeyUpperThreshold || '∞' }
+                    </>
+                  )}
+                >
+                  <td className="pr-2 text-gray-500 text-right">
+                    { name }
+                  </td>
+                </Popover>
                 <td className={`pr-2 ${(dataKeyLowerThreshold || dataKeyUpperThreshold) && ((value <= dataKeyLowerThreshold) || (value >= dataKeyUpperThreshold)) ? 'text-red-700' : ''}`}>
                   {
-                    value !== undefined
-                      ? `${value}${unit}` : '-'
-                  }
+                      value !== undefined
+                        ? `${value}${unit}` : '-'
+                    }
                 </td>
                 <td className="text-gray-500">
                   { time || '-' }
