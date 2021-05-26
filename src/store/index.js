95d995d36843e462774bf6a17a459aee1f82e40b
ginoscott <ginoscottdev@gmail.com>
2021-05-26 01:04:34 -0400

    switch to using official redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducers';
 
-export default createStore(
-  reducer,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+export default configureStore({
+  reducer: reducer,
+});
