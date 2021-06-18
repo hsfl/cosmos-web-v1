f34ad0c2d6039e7c3484704b55623eb9d48f6e9d
ginoscott <ginoscottdev@gmail.com>
2021-06-18 16:27:35 -0400

    fixing more eslint warnings

--- src/components/Dashboard/CosmosPanel/RecursiveProperty.jsx
+++ src/components/Dashboard/CosmosPanel/RecursiveProperty.jsx
@@ -8,6 +8,75 @@ const RecursiveProperty = ({
 }) => {
   const [isHovered, setIsHovered] = useState(false);
 
+  const buildNamespaceName = (key) => {
+    // Leave out the name of the root
+    if (isRoot) {
+      return key;
+    }
+
+    // Check if key is an integer
+    // (Warning: this can potentially result in some wacky results
+    // depending on the value of key, but it's very fast.
+    // eg: [] [2], [undefined], "", true, false)
+    if (!Number.isNaN(+key)) {
+      // Arrays use []
+      return `${name}[${key}]`;
+    }
+    // Objects use '.' as separators
+    return `${name}.${key}`;
+  };
+
+  const buildRecursiveTree = () => (
+    // A leaf node, base case
+    (Object.keys(data).length === 1 && !isRoot) ? (
+      <>
+        <button
+          type="button"
+          onClick={() => callBack(name)}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {title}
+        </button>
+        <span className={`typeofnamespan ${isHovered ? 'isHovered' : ''}`}>{ data.type_of_name }</span>
+      </>
+    ) : (
+      // An Object, can click to expand
+      <ExpandableProperty
+        title={title.toString()}
+        expanded={isRoot}
+        name={name}
+        type={data.type_of_name}
+        callBack={callBack}
+      >
+        {
+          Object.keys(data).reduce((acc, key) => {
+            // Don't display the type_of_name
+            if (key === 'type_of_name') {
+              return acc;
+            }
+
+            // Construct namespace name for child
+            const newName = buildNamespaceName(key);
+
+            // Recursion
+            acc.push(<RecursiveProperty
+              key={newName}
+              data={data[key]}
+              title={key}
+              isRoot={false}
+              name={newName}
+              callBack={callBack}
+            />);
+
+            // Return accumulator, continue loop
+            return acc;
+          }, [])
+        }
+      </ExpandableProperty>
+    )
+  );
+
   return (
     <div className="recursivePropertyContainer">
       <style>
@@ -33,69 +102,7 @@ const RecursiveProperty = ({
       </style>
       {
         (data !== undefined && data !== null) ? (
-          // A leaf node, base case
-          (Object.keys(data).length === 1 && !isRoot) ? (
-            <>
-              <button
-                type="button"
-                onClick={() => callBack(name)}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                {title}
-              </button>
-              <span className={`typeofnamespan ${isHovered ? 'isHovered' : ''}`}>{ data.type_of_name }</span>
-            </>
-          ) : (
-            // An Object, can click to expand
-            <ExpandableProperty
-              title={title.toString()}
-              expanded={isRoot}
-              name={name}
-              type={data.type_of_name}
-              callBack={callBack}
-            >
-              {
-                Object.keys(data).reduce((acc, key) => {
-                  // Don't display the type_of_name
-                  if (key === 'type_of_name') {
-                    return acc;
-                  }
-
-                  // Construct namespace name for child
-                  const newName = (isRoot) ? (
-                    // Leave out the name of the root
-                    key
-                  ) : (
-                    // Check if key is an integer
-                    // (Warning: this can potentially result in some wacky results
-                    // depending on the value of key, but it's very fast.
-                    // eg: [] [2], [undefined], "", true, false)
-                    !Number.isNaN(+key) ? (
-                      // Arrays use []
-                      `${name}[${key}]`
-                    ) : (
-                      // Objects use '.' as separators
-                      `${name}.${key}`
-                    )
-                  );
-
-                  // Recursion
-                  acc.push(<RecursiveProperty
-                    key={newName}
-                    data={data[key]}
-                    title={key}
-                    isRoot={false}
-                    name={newName}
-                    callBack={callBack}
-                  />);
-
-                  // Return accumulator, continue loop
-                  return acc;
-                }, [])
-              }
-            </ExpandableProperty>
-          )
+          buildRecursiveTree()
         ) : (
           // undefined or null value
           <div>no value</div>
